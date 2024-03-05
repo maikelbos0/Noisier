16f269de49d64f8bf0a474d7a97459a2cba2cb8e
Maikel Bos <maikel.bos0@gmail.com>
2024-03-05 19:31:34 +0100

    Add frequency based on note/octave

---
+++ Noisier/Frequency.cs
@@ -0,0 +1,8 @@
+﻿namespace Noisier;
+
+public record Frequency(Note Note, int Octave) {
+    public const double A4 = 440;
+    public const int NotesPerOctave = 12;
+
+    public double Value => A4 * Math.Pow(2, ((Octave - 4) * NotesPerOctave + (int)Note - (int)Note.A) / (double)NotesPerOctave);
+}
---
+++ Noisier/Note.cs
@@ -0,0 +1,16 @@
+﻿namespace Noisier;
+
+public enum Note {
+    C,
+    CSharp,
+    D,
+    DSharp,
+    E,
+    F,
+    FSharp,
+    G,
+    GSharp,
+    A,
+    ASharp,
+    B
+}
