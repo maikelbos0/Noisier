c930edcfd17d8dc22fcc84674e9120c2c4ff21a4
Maikel Bos <maikel.bos0@gmail.com>
2024-03-21 20:05:43 +0100

    Add 'horn' waveform

--- Noisier.Tests/WaveformCalculatorsTests.cs
+++ Noisier.Tests/WaveformCalculatorsTests.cs
@@ -26,4 +26,16 @@ public class WaveformCalculatorsTests {
     public void Piano(double timePoint, double frequency, double expectedAmplitude) {
         Assert.Equal(expectedAmplitude, WaveformCalculators.Piano()(timePoint, frequency), 2);
     }
+
+    [Theory]
+    [InlineData(0, 10, 0.72)]
+    [InlineData(0.005, 10, 0.51)]
+    [InlineData(0.02, 10, 0.49)]
+    [InlineData(0.025, 10, 0.3)]
+    [InlineData(0.05, 10, -0.58)]
+    [InlineData(0.075, 10, 0.55)]
+    [InlineData(0.1, 10, 0.72)]
+    public void Horn(double timePoint, double frequency, double expectedAmplitude) {
+        Assert.Equal(expectedAmplitude, WaveformCalculators.Horn()(timePoint, frequency), 2);
+    }
 }
--- Noisier/WaveformCalculators.cs
+++ Noisier/WaveformCalculators.cs
@@ -1,7 +1,14 @@
 ﻿namespace Noisier;
 
 public static class WaveformCalculators {
-    public static WaveformCalculator Sine() => (timePoint, frequency) => Math.Sin(timePoint * frequency * 2 * Math.PI);
+    public static WaveformCalculator Sine() => (timePoint, frequency) 
+        => Math.Sin(timePoint * frequency * 2 * Math.PI);
 
-    public static WaveformCalculator Piano() => (timePoint, frequency) => Math.Pow(Math.Sin(timePoint * frequency * 2 * Math.PI), 3) + Math.Sin((timePoint * frequency * 2 + 2 / 3.0) * Math.PI);
+    public static WaveformCalculator Piano() => (timePoint, frequency) 
+        => Math.Pow(Math.Sin(timePoint * frequency * 2 * Math.PI), 3) 
+            + Math.Sin((timePoint * frequency * 2 + 2 / 3.0) * Math.PI);
+
+    public static WaveformCalculator Horn() => (timePoint, frequency)
+        => Math.Pow(Math.Sin((timePoint * frequency * 2 + 2 / 3.0) * Math.PI), 3)
+            + 0.5 * Math.Pow(Math.Sin((timePoint * frequency * 2 - 7 / 8.0) * Math.PI), 2);
 }
