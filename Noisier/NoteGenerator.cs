d4ba4e87b4edad904600748c32421ab47726f5ca
Maikel Bos <maikel.bos0@gmail.com>
2024-08-01 19:43:51 +0200

    Add basic note generator concept

---
+++ Noisier/NoteGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace Noisier;
+
+public class NoteGenerator {
+    private const int iterations = 1000;
+    private const uint baseOctave = 4;
+    private const uint duration = 8;
+    private const uint denominator = 4;
+
+    private readonly static byte[] salt = Enumerable.Range(0, 20).Select(i => (byte)(8 + i * 4)).ToArray();
+
+    public static IEnumerable<Note> Generate(string value) {
+        using var pbkdf2 = new Rfc2898DeriveBytes(value, salt, iterations, HashAlgorithmName.SHA256);
+
+        var note = GenerateNote(pbkdf2, new Fraction(0, denominator));
+
+        while (note.Position.Value + note.Duration.Value <= duration) {
+            yield return note;
+
+            note = GenerateNote(pbkdf2, note.Position + note.Duration);
+        }
+    }
+
+    private static Note GenerateNote(Rfc2898DeriveBytes pbkdf2, Fraction position) {
+        return new Note(position, new Fraction((uint)pbkdf2.GetBytes(1).Single() % 4 + 1, denominator), new Pitch(PitchClass.C, 4));
+    }
+}
--- Noisier/Program.cs
+++ Noisier/Program.cs
@@ -11,27 +11,34 @@ waveCreator.Tracks.Add(new() {
     Positions = [new(1, 1)],
     WaveformCalculator = WaveformCalculators.Piano(),
     VolumeCalculator = VolumeCalculators.LinearDecrease(),
-    Notes = [
-        new(new(0, 4), new(1, 1), new(PitchClass.C, 3), new(PitchClass.E, 3), new(PitchClass.G, 3)),
-        new(new(4, 4), new(1, 1), new Pitch(PitchClass.D, 3)),
-        new(new(5, 4), new(1, 1), new Pitch(PitchClass.E, 3)),
-        new(new(6, 4), new(1, 1), new Pitch(PitchClass.F, 3)),
-        new(new(7, 4), new(1, 1), new Pitch(PitchClass.G, 3)),
-        new(new(8, 4), new(1, 1), new(PitchClass.A, 3), new(PitchClass.C, 4), new(PitchClass.E, 4)),
-        new(new(12, 4), new(1, 1), new Pitch(PitchClass.B, 3)),
-        new(new(14, 4), new(1, 1), new(PitchClass.C, 4), new(PitchClass.E, 4), new(PitchClass.G, 4)),
-    ]
+    Notes = NoteGenerator.Generate("test").ToList()
 });
-waveCreator.Tracks.Add(new() {
-    Positions = [new(1, 1)],
-    WaveformCalculator = WaveformCalculators.Horn(),
-    VolumeCalculator = VolumeCalculators.Sine(),
-    Notes = [
-        new(new(0, 4), new(2, 1), new Pitch(PitchClass.C, 2)),
-        new(new(8, 4), new(1, 1), new Pitch(PitchClass.A, 2)),
-        new(new(12, 4), new(1, 1), new Pitch(PitchClass.C, 3)),
-    ]
-});
+
+//waveCreator.Tracks.Add(new() {
+//    Positions = [new(1, 1)],
+//    WaveformCalculator = WaveformCalculators.Piano(),
+//    VolumeCalculator = VolumeCalculators.LinearDecrease(),
+//    Notes = [
+//        new(new(0, 4), new(1, 1), new(PitchClass.C, 3), new(PitchClass.E, 3), new(PitchClass.G, 3)),
+//        new(new(4, 4), new(1, 1), new Pitch(PitchClass.D, 3)),
+//        new(new(5, 4), new(1, 1), new Pitch(PitchClass.E, 3)),
+//        new(new(6, 4), new(1, 1), new Pitch(PitchClass.F, 3)),
+//        new(new(7, 4), new(1, 1), new Pitch(PitchClass.G, 3)),
+//        new(new(8, 4), new(1, 1), new(PitchClass.A, 3), new(PitchClass.C, 4), new(PitchClass.E, 4)),
+//        new(new(12, 4), new(1, 1), new Pitch(PitchClass.B, 3)),
+//        new(new(14, 4), new(1, 1), new(PitchClass.C, 4), new(PitchClass.E, 4), new(PitchClass.G, 4)),
+//    ]
+//});
+//waveCreator.Tracks.Add(new() {
+//    Positions = [new(1, 1)],
+//    WaveformCalculator = WaveformCalculators.Horn(),
+//    VolumeCalculator = VolumeCalculators.Sine(),
+//    Notes = [
+//        new(new(0, 4), new(2, 1), new Pitch(PitchClass.C, 2)),
+//        new(new(8, 4), new(1, 1), new Pitch(PitchClass.A, 2)),
+//        new(new(12, 4), new(1, 1), new Pitch(PitchClass.C, 3)),
+//    ]
+//});
 
 waveCreator.Create(path);
 
