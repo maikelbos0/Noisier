3f3eba9dccb7a0e9aaf71202269201099d49d527
Maikel Bos <maikel.bos0@gmail.com>
2024-08-01 19:55:37 +0200

    Generator now with pitch

--- Noisier/NoteGenerator.cs
+++ Noisier/NoteGenerator.cs
@@ -4,25 +4,34 @@ namespace Noisier;
 
 public class NoteGenerator {
     private const int iterations = 1000;
-    private const uint baseOctave = 4;
+    private const int baseOctave = 4;
     private const uint duration = 8;
     private const uint denominator = 4;
+    private const uint bandwidth = 2;
 
     private readonly static byte[] salt = Enumerable.Range(0, 20).Select(i => (byte)(8 + i * 4)).ToArray();
 
-    public static IEnumerable<Note> Generate(string value) {
+    public static IEnumerable<Note> Generate(string value, IList<PitchClass> scale) {
         using var pbkdf2 = new Rfc2898DeriveBytes(value, salt, iterations, HashAlgorithmName.SHA256);
 
-        var note = GenerateNote(pbkdf2, new Fraction(0, denominator));
+        var note = GenerateNote(pbkdf2, new Fraction(0, denominator), scale);
 
         while (note.Position.Value + note.Duration.Value <= duration) {
             yield return note;
 
-            note = GenerateNote(pbkdf2, note.Position + note.Duration);
+            note = GenerateNote(pbkdf2, note.Position + note.Duration, scale);
         }
     }
 
-    private static Note GenerateNote(Rfc2898DeriveBytes pbkdf2, Fraction position) {
-        return new Note(position, new Fraction((uint)pbkdf2.GetBytes(1).Single() % 4 + 1, denominator), new Pitch(PitchClass.C, 4));
+    private static Note GenerateNote(Rfc2898DeriveBytes pbkdf2, Fraction position, IList<PitchClass> scale) {
+        var pitchClassIndex = (int)(pbkdf2.GetBytes(1).Single() % (scale.Count * bandwidth) - (scale.Count * bandwidth) / 2);
+        var octave = baseOctave + pitchClassIndex / scale.Count;
+        var pitchClass = scale[(pitchClassIndex + scale.Count) % scale.Count];
+
+        if (pitchClassIndex < 0) {
+            octave--;
+        }
+
+        return new Note(position, new Fraction((uint)pbkdf2.GetBytes(1).Single() % 4 + 1, denominator), new Pitch(pitchClass, octave));
     }
 }
---
+++ Noisier/Scales.cs
@@ -0,0 +1,13 @@
+﻿namespace Noisier;
+
+public static class Scales {
+    public static IList<PitchClass> CMajor => [
+        PitchClass.C,
+        PitchClass.D,
+        PitchClass.E,
+        PitchClass.F,
+        PitchClass.G,
+        PitchClass.A,
+        PitchClass.B
+    ];
+}
