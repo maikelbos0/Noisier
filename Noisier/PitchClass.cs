053553fde858cd1e334bf1da55968d9d9ee61e7e
Maikel Bos <maikel.bos0@gmail.com>
2024-08-01 19:57:13 +0200

    Add flats

--- Noisier/PitchClass.cs
+++ Noisier/PitchClass.cs
@@ -1,16 +1,21 @@
 ﻿namespace Noisier;
 
 public enum PitchClass {
-    C,
-    CSharp,
-    D,
-    DSharp,
-    E,
-    F,
-    FSharp,
-    G,
-    GSharp,
-    A,
-    ASharp,
-    B
+    C = 0,
+    CSharp = 1,
+    DFlat = CSharp,
+    D = 2,
+    DSharp = 3,
+    EFlat = DSharp,
+    E = 4,
+    F = 5,
+    FSharp = 6,
+    GFlat = FSharp,
+    G = 7,
+    GSharp = 8,
+    AFlat = GSharp,
+    A = 9,
+    ASharp = 10,
+    BFlat = ASharp,
+    B = 11
 }
