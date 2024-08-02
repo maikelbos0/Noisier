7ed8b5e03b7121c00f4762304ea53b88cc763a87
Maikel Bos <maikel.bos0@gmail.com>
2024-08-02 13:21:35 +0200

    Add offset tests

--- Noisier.Tests/NoteGeneratorTests.cs
+++ Noisier.Tests/NoteGeneratorTests.cs
@@ -9,14 +9,17 @@ public class NoteGeneratorTests {
     [InlineData(0, 100, 0, 0)]
     [InlineData(0, 100, 99, 99)]
     [InlineData(0, 100, 50, 150)]
+    [InlineData(500, 600, 550, 150)]
 
     [InlineData(0, 500, 0, 0, 0)]
     [InlineData(0, 500, 499, 1, 243)]
     [InlineData(0, 500, 250, 43, 242)]
+    [InlineData(2500, 3000, 2750, 43, 242)]
 
     [InlineData(0, 100000, 0, 0, 0, 0)]
     [InlineData(0, 100000, 99999, 1, 134, 159)]
     [InlineData(0, 100000, 50000, 0, 195, 80)]
+    [InlineData(500000, 600000, 550000, 0, 195, 80)]
     public void GetValueReturnsValueBetweenMinimumAndMaximum(uint includingMinimum, uint excludingMaximum, uint expectedValue, params int[] bytes) {
         var deriveBytes = Substitute.For<DeriveBytes>();
         var subject = new NoteGenerator("", Scales.CMajor) {
